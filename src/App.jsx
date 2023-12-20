8d8c055a4ec7c0034dee1036bfae4abd64084720
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2023-12-19 21:18:40 -0600

    Implementing Google Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//app.jsx
+
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { SERVER_URL } from './services/SERVER_URL'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleLogin = async (googleData) => {
+    try {
+      const response = await axios.post(`${SERVER_URL}/auth/google-login`, {
+        tokenId: googleData.credential,
+      });
+      const { authToken } = response.data;
+      localStorage.setItem("authToken", authToken);
+      // Update UI or redirect as needed
+    } catch (error) {
+      console.error("Login Error", error.response);
+    }
+  };
 
   return (
-    <>
+    <GoogleOAuthProvider clientId="369527887188-jc5rpas4gfd0e4teldg7qt54h86u4j5n.apps.googleusercontent.com">
       <div>
         <h1>Welcome to my Portfolio</h1>
+        <GoogleLogin
+          onSuccess={handleLogin}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
       </div>
-    </>
-  )
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="369527887188-jc5rpas4gfd0e4teldg7qt54h86u4j5n.apps.googleusercontent.com">...</GoogleOAuthProvider>;
     <App />
   </React.StrictMode>,
 )
