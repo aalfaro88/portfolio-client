a906cfec054624293b7dce0bb1b3ec8753746fc3
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2024-02-04 11:08:08 -0600

    Contact info done

--- src/components/front-page/ContactInfo.jsx
+++ src/components/front-page/ContactInfo.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import './ContactInfo.css'
+import React from 'react';
+import './ContactInfo.css';
 
 const ContactInfo = () => {
   return (
     <div className='contact-info-section'>
-        <h1>WORK IN PROGRESS: CONTACT INFO</h1>
+      <h1>Contact Information</h1>
+      <div className='contact-links'>
+        <a
+          href='https://www.linkedin.com/in/alberto-alfaro/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='contact-link'
+        >
+          <img
+            src='https://img.icons8.com/ios-filled/50/linkedin.png' // LinkedIn Icon
+            alt='LinkedIn'
+            className='contact-icon'
+          />
+          <p>
+              LinkedIn
+          </p>
+        </a>
+        <a
+          href='https://github.com/aalfaro88'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='contact-link'
+        >
+          <img
+            src='https://img.icons8.com/ios-filled/50/github.png' // GitHub Icon
+            alt='GitHub'
+            className='contact-icon'
+          />
+          <p>
+              GitHub
+          </p>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactInfo
+export default ContactInfo;
